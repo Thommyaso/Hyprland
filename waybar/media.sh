9bf0a352a146a0e284cff6939d01c321dddd862f
Tomasz <tomgaek@gmail.com>
2026-07-25 23:00:40 +0100

    Get rid of mpris

---
+++ waybar/media.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+status="$(playerctl status 2>/dev/null)"
+text="$(playerctl metadata --format '{{artist}} - {{title}}' 2>/dev/null)"
+
+pwd="$(pwd)"
+
+case "$status" in
+    Playing)
+        class="playing"
+        ;;
+    Paused)
+        class="paused"
+        ;;
+    *)
+        class="stopped"
+        ;;
+esac
+
+printf '{"text":"%s","class":"%s", "pwd": "%s"}\n' \
+    "${text//\"/\\\"}" \
+    "$class" \
+    "$pwd" >> "/tmp/path.txt"
+
+printf '{"text":"%s","class":"%s", "pwd": "%s"}\n' \
+    "${text//\"/\\\"}" \
+    "$class" \
+    "$pwd"
